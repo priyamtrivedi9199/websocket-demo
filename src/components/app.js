6d102ef4d0d78f06881ce90c967d40d8f7f6656d
priyamtrivedi9199 <trivedipriyam9199@gmail.com>
2021-07-08 19:39:03 +0530

    adding server changes

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,25 @@
+import { getDefaultNormalizer } from "@testing-library/dom";
 import React from "react";
-import { Connector } from "../components/connector";
 var WebSocket = require("isomorphic-ws");
-
 const App = () => {
   const w = new WebSocket("wss://api-pub.bitfinex.com/ws/2");
-
-  w.onOpen = function () {
-    console.log(w)
+  const [socket, setSocket] = React.useState();
+  React.useEffect(() => {}, []);
+  w.onopen = function (msg) {
+    w.send(msg);
   };
+
+  w.onmessage = function (msg) {
+    console.log(msg.data)
+  };
+
+  function GetData(data) {
+    return console.log(data)
+  }
+
   return (
     <div>
-      <Connector />
+      
     </div>
   );
 };
--- src/server/app.js
+++ src/server/app.js
@@ -2,30 +2,24 @@ var express = require("express");
 var cors = require("cors");
 var path = require("path");
 var ws = require("ws");
-
 const app = express();
 app.use(cors());
 app.options("*", cors());
-// app.urlencoded()
 const w = new ws("wss://api-pub.bitfinex.com/ws/2");
 const server = require("http").createServer(app);
 const port = 6700;
-
-app.use ((req, res, next) => {
-  var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-  console.log(fullUrl)
-    next();
-});
-server.listen(port, () => {
-  console.log("Server listening at port %d", port);
-});
-
-w.on("message", (msg) => console.log(msg));
 
 let msg = JSON.stringify({
   event: "subscribe",
   channel: "ticker",
   symbol: "tBTCUSD",
 });
+app.get("/", (req, res) => {
+  w.on("message", (msg) => res.send(msg));
+  // w.on("message", (msg) => msg);
+  w.on("open", () => res.send(msg));
+});
 
-w.on("open", () => w.send(msg));
+server.listen(port, (req, res) => {
+  console.log("Server listening at port %d", port);
+});
