2e54aa9f66f4e047caad8fbe31ba196350fa72b7
priyamtrivedi9199 <trivedipriyam9199@gmail.com>
2021-07-13 19:07:52 +0530

    adding fixed figures for data

--- src/components/app.js
+++ src/components/app.js
@@ -13,7 +13,7 @@ import {
   Side,
   BitCoinIcon,
   Container,
-  Wrapper
+  Wrapper,
 } from "../components/component.styles";
 
 const Ticker = connect((s) => ({ ticker: s.ticker }))((props) => {
@@ -24,41 +24,52 @@ const Ticker = connect((s) => ({ ticker: s.ticker }))((props) => {
   );
   const [
     CHANNEL_ID,
-    [DAILY_CHANGE, DAILY_CHANGE_PERC, LAST_PRICE, VOLUME, HIGH, LOW],
+    [
+      BID,
+      BID_SIZE,
+      ASK,
+      ASK_SIZE,
+      DAILY_CHANGE,
+      DAILY_CHANGE_PERC,
+      LAST_PRICE,
+      VOLUME,
+      HIGH,
+      LOW,
+    ],
   ] = Array.isArray(ticker) ? ticker : empty_ticker;
   const [connectionStatus, setConnectionStatus] = useState(true);
 
   useEffect(() => {
     wsconnect({ saveTicker, setConnectionStatus, connectionStatus });
   }, [connectionStatus]);
-  const startConnection = () => !connectionStatus && setConnectionStatus(true)
-  const stopConnection = () => connectionStatus && setConnectionStatus(false)
+  const startConnection = () => !connectionStatus && setConnectionStatus(true);
+  const stopConnection = () => connectionStatus && setConnectionStatus(false);
   return (
-      <Wrapper>
-        <Container>
-          <BitCoinIcon>
-            <IoLogoBitcoin />
-          </BitCoinIcon>
-          <Side>
-            <h4>BTC/USD</h4>
-            <Line>VOL {VOLUME && numberWithCommas(VOLUME.toFixed(2))} USD</Line>
-            <Line>Low {LOW && numberWithCommas(LOW.toFixed(1))}</Line>
-          </Side>
-          <Side>
-            <h4>{LAST_PRICE && numberWithCommas(LAST_PRICE.toFixed(1))}</h4>
-            <Line>
-              <span className={DAILY_CHANGE_PERC < 0 ? `red` : "green"}>
-                {DAILY_CHANGE && numberWithCommas(DAILY_CHANGE.toFixed(2))}
-                {DAILY_CHANGE_PERC < 0 ? <FaCaretDown /> : <FaCaretUp />}(
-                {DAILY_CHANGE_PERC}%)
-              </span>
-            </Line>
-            <Line>High {HIGH && numberWithCommas(HIGH.toFixed(1))}</Line>
-          </Side>
-        </Container>
-        <StyledButton onClick={startConnection}>Connect</StyledButton>
-        <StyledButton onClick={stopConnection}>Disconnect</StyledButton>
-      </Wrapper>
+    <Wrapper>
+      <Container>
+        <BitCoinIcon>
+          <IoLogoBitcoin />
+        </BitCoinIcon>
+        <Side>
+          <h4>BTC/USD</h4>
+          <Line>VOL {VOLUME && numberWithCommas(VOLUME.toFixed(2))} USD</Line>
+          <Line>Low {LOW }</Line>
+        </Side>
+        <Side>
+          <h4>{LAST_PRICE && numberWithCommas(LAST_PRICE.toFixed(1))}</h4>
+          <Line>
+            <span className={DAILY_CHANGE_PERC < 0 ? `red` : "green"}>
+              {DAILY_CHANGE && numberWithCommas(DAILY_CHANGE.toFixed(2))}
+              {DAILY_CHANGE_PERC < 0 ? <FaCaretDown /> : <FaCaretUp />}(
+              {DAILY_CHANGE_PERC}%)
+            </span>
+          </Line>
+          <Line>High {HIGH}</Line>
+        </Side>
+      </Container>
+      <StyledButton onClick={startConnection}>Connect</StyledButton>
+      <StyledButton onClick={stopConnection}>Disconnect</StyledButton>
+    </Wrapper>
   );
 });
 
